5daf1789a238a8a46768916b67e5964c6ef16138
Shin <nijanitaoma@gmail.com>
2025-03-04 20:30:25 +0200

    particles wip

--- src/graphics/particles.cpp
+++ src/graphics/particles.cpp
@@ -4,41 +4,56 @@
 #include "../../headers/systems/camera.h"
 #include "../../headers/systems/renderManager.h"
 
-void Particles::GenerateParticles()
+bool Particles::GenerateParticles()
 {
     glEnable(GL_PROGRAM_POINT_SIZE);
 
     // filling path with current positions of particles
     FillParticlesPoints();
     
+    _setup.type = GL_DYNAMIC_COPY | GL_DYNAMIC_DRAW;
+    OpenglBackend::BindModelVBO(_setup);
+
     Shader computeShader;
     computeShader.LoadComputeShader("particles.comp");
     computeShader.UseShader();
 
     // ssbo
+    if(_setup.vertices.size() == 0)
+    {
+        std::cout << "Can't generate particles as vertices vector is empty!";
+        return false;
+    }
+
     const uint32_t verticesBindNumber = 1;
     SSBOBindVec4 bindData;
     bindData.binding = &verticesBindNumber;
     bindData.ssboId = &_ssboId;
     bindData.data = _setup.vertices.data();
-    bindData.size = sizeof(_setup.vertices[0]) * _setup.vertices.size();
-    OpenglBackend::CreateSSBO(bindData);
+    bindData.size = sizeof(glm::vec4) * _setup.vertices.size();
+    bindData.type = GL_DYNAMIC_COPY;
+    if(OpenglBackend::CreateSSBO(bindData) == ErrorCodes_Backend::ERROR_SSBO_CREATION)
+    {
+        std::cout << "Unable to initialize particles\n";
+        return false;
+    }
 
     // vertex/frag shaders
     Shader mainShader;
     mainShader.LoadShaders("particles.vert", "particles.frag");
     mainShader.UseShader();
     
-
-    OpenglBackend::BindModelVBO(_setup);
     
     glm::mat4 model = glm::mat4(1.0f);
-    model = glm::translate(model, glm::vec3(-35.0f, 5.0f, 35.0f));
+    // model = glm::translate(model, glm::vec3(-35.0f, 5.0f, 35.0f));
     // model = glm::scale(model, glm::vec3(50.0f));
     mainShader.SetMat4x4("model", model);
 
     RenderManager::AddShaderByType(std::move(computeShader), RenderPassType::RENDER_PARTICLES_COMP);
     RenderManager::AddShaderByType(std::move(mainShader), RenderPassType::RENDER_PARTICLES);
+
+
+    return true;
 }
 
 void Particles::FillParticlesPoints()
@@ -62,10 +77,26 @@ void Particles::FillParticlesPoints()
 
 }
 
+void Particles::Prepare()
+{
+    if(!GenerateParticles())
+    {
+        std::cout << "Unable to generate particles\n";
+        return;
+    }
+
+    // to do: imgui
+}
+
+void Particles::RenderParticles()
+{
+    ExecuteCompShader();
+
+    DrawParticles();
+}
+
 void Particles::DrawParticles()
 {
-    glBindVertexArray(_setup.VAO);
-    ExecuteCompShader();
     auto shaderIt = RenderManager::_shaderTypes.find(RenderPassType::RENDER_PARTICLES);
     if(shaderIt != RenderManager::_shaderTypes.end())
     {
@@ -73,7 +104,7 @@ void Particles::DrawParticles()
         shaderIt->second.SetMat4x4("view", Camera::GetMVP().view);
         shaderIt->second.SetMat4x4("projection", Camera::GetMVP().projection);
     }
-
+    glBindVertexArray(_setup.VAO);
     glDrawArrays(GL_POINTS, 0, _setup.vertices.size());
     glBindVertexArray(0);
 }
@@ -84,8 +115,12 @@ void Particles::ExecuteCompShader()
     if(shaderIt != RenderManager::_shaderTypes.end())
     {
         shaderIt->second.UseShader();
+        glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, _ssboId);
         glDispatchCompute(_numberOfParticles, 1, 1);
         glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+
+        // passing data to the vbo
+        glCopyNamedBufferSubData(_ssboId, _setup.VBO, 0, 0, _numberOfParticles);
     }
 
 }
